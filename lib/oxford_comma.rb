1054e0719eb4326c2f30f4ac31e0b30ea69779fd
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-03-31 01:48:32 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    array.join
+  elsif array.count == 2
+    array.join(" and ")
+  elsif array.count >= 3
+    array.push("and #{array.pop}")
+    array.join(", ")
+  end
 end
