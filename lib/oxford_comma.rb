a66592cf682f80eb73a099b0d10a79fcbb94c3b2
harmonious-innovator-3896 <kevin20130209@hotmail.com>
2019-03-31 01:40:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
+  if array.count == 1
+    array.split
+  elsif array.count == 2
+    array.join(" and ")
+  elsif array.count == 3
+    array.join(", ")
+  elsif array.count > 3
+    array.last = 'and ' + array.last
+    array.join(", ")
+  end
 
+
+    
 end
